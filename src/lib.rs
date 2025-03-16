4bac881fd9e24b9cc15206a9e846b3e5f726ce57
artem <work.aab.25122001@gmail.com>
2025-03-16 20:07:06 +0000

    progress

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,46 @@
+#![feature(generic_const_exprs)]
+
+struct Concat<L,R,const LL: usize, const RL: usize> {
+    left: L,
+    right: R,
+}
+
+impl<L,R, const I: usize, const LL: usize, const RL: usize> NeuralNetwork<I,{LL + RL}> for Concat<L,R,LL,RL> 
+    where L: NeuralNetwork<I,LL>, R: NeuralNetwork<I,RL>
+{
+    fn forward(&mut self, x: &[f64; I]) -> [f64; LL + RL] {
+        let mut result = [0.0; {LL + RL}];
+        let left = self.left.forward(x);
+        let right = self.right.forward(x);
+        for i in 0..LL {
+            result[i] = left[i];
+        }
+        for i in 0..RL {
+            result[i + LL] = right[i];
+        }
+        result
+    }
+    
+    fn backward(&mut self, x: &[f64; I], errors: [f64; LL + RL], temperature: f64) -> [f64; I] {
+        let mut left_errors = [0.0; LL];
+        let mut right_errors = [0.0; RL];
+        for i in 0..LL {
+            left_errors[i] = errors[i];
+        }
+        for i in 0..RL {
+            right_errors[i] = errors[i + LL];
+        }
+        let left = self.left.backward(x, left_errors, temperature);
+        let right = self.right.backward(x, right_errors, temperature);
+
+        let mut result = [0.0; I];
+        for i in 0..I {
+            result[i] = left[i] + right[i];
+        }
+        result
+    }
+}
+
 struct Chain<F, S, const M: usize> {
     first: F,
     second: S,
@@ -11,18 +54,29 @@ impl<F,S, const I: usize,const M: usize, const O: usize> NeuralNetwork<I,O> for 
         let errors = self.second.backward(&self.first.forward(x), errors, temperature);
         self.first.backward(x, errors, temperature)
     }
-    fn chain<const SZ: usize>(self, other: impl NeuralNetwork<O, SZ>) -> impl NeuralNetwork<I, SZ> where Self: Sized {
-        Chain { first: self, second: other }
-    }
 }
 
-pub trait NeuralNetwork<const INP: usize, const OUT: usize> {
+pub trait NeuralNetwork<const INP: usize, const OUT: usize> 
+    where [(); INP]:, [(); OUT]:
+{
     fn forward(&mut self, x: &[f64; INP]) -> [f64; OUT];
     fn backward(&mut self, x: &[f64; INP], errors: [f64; OUT], temperature: f64) -> [f64; INP];
+    /// this is for stuff like transformers, norms, etc
+    /// it is called by forward pass, default returns argument
+    fn preprocess(&mut self, x: &[f64; INP]) -> [f64; INP] {
+        *x
+    }
     fn chain<const SZ: usize>(self, other: impl NeuralNetwork<OUT, SZ>) -> impl NeuralNetwork<INP, SZ> where Self: Sized {
         Chain { first: self, second: other }
     }
+    fn concat<const RSZ: usize>(self,other: impl NeuralNetwork<INP,RSZ>) -> impl NeuralNetwork<INP, {OUT + RSZ}> 
+        where Self: Sized, [();OUT + RSZ]:
+    {
+        Concat { left: self, right: other }
+    }
 }
+
+pub mod attention;
 
 pub mod fc_layer {
     use super::*;
@@ -94,71 +148,142 @@ pub mod fc_layer {
     
 }
 
-pub mod lnn_layer {
+/// Module for lnn layer with weighted recap and self attention
+pub mod lnn_exp {
     use super::*;
     use std::ops::Range;
     use rand::Rng;
     
-    pub struct LNNLayer<const INP: usize, const OUT: usize> {
+    /// DEPTH must be greater than 1
+    pub struct LNNLayer<const INP: usize, const OUT: usize, const DEPTH: usize>
+    {   
+        // attention: [f64; INP],
         weights: [[f64; INP]; OUT],
         bias: [f64; OUT],
-        hidden_state: [f64; OUT],
-        alpha: f64, // Governs temporal memory effect
+
+        /// this is the current step in the hidden state vector, always less than DEPTH
+        /// it is used to determine the index of the hidden state vector to write to
+        /// an index to the ring buffer
+        current_step: usize,
+        hidden_state: [[f64; INP]; DEPTH],
+
+        /// Governs the amount of information and the way to keep it 
+        /// from the hidden state vector
+        alpha_vector: [f64; INP],
+
         activation: fn(f64) -> f64,
         activation_derivative: Option<Box<dyn Fn(f64) -> f64>>,
     }
     
-    impl<const I: usize, const O: usize> LNNLayer<I, O> {
-        pub fn new(activation: fn(f64) -> f64, value_range: Range<f64>, alpha: f64) -> Self {
+    impl<const I: usize, const O: usize, const D: usize> LNNLayer<I, O, D> {
+        pub fn random(activation: fn(f64) -> f64, value_range: Range<f64>) -> Self {
             let mut rng = rand::rng();
             
             LNNLayer {
                 weights: [[rng.random_range(value_range.clone()); I]; O],
-                bias: [rng.random_range(value_range); O],
-                hidden_state: [0.0; O],
-                alpha,
+                bias: [rng.random_range(value_range.clone()); O],
+                // attention: [1.0; I],
+
+                // q_weights: [[rng.random_range(value_range.clone()); I]; I],
+                // k_weights: [[rng.random_range(value_range.clone()); I]; I],
+                // v_weights: [[rng.random_range(value_range.clone()); I]; I],
+
+                current_step: 0,
+                hidden_state: [[0.0; I]; D],
+
+                alpha_vector: [rng.random_range(value_range.clone()); I],
                 activation,
                 activation_derivative: None,
             }
         }
-        
+
         pub fn with_derivative(mut self, derivative: impl Fn(f64) -> f64 + 'static) -> Self {
             self.activation_derivative = Some(Box::new(derivative));
             self
         }
 
-        pub fn derivative(&self, x: f64) -> f64 {
+        fn derivative(&self, x: f64) -> f64 {
             if let Some(derivative) = &self.activation_derivative {
                 return derivative(x);
             }
             let step = 0.001;
             ((self.activation)(x + step) - (self.activation)(x - step)) / (2.0 * step)
         }
+
+        /// the absolute length of the hidden state vector
+        fn alpha(&self) -> f64 {
+            self.alpha_vector.iter().map(|x| x * x).sum::<f64>().sqrt()
+        }
+
+        /// get the indices of the ring buffer: (head, tail)
+        fn get_ring_indices(&self) -> (usize, usize) {
+            let from = self.current_step % D;
+            let to = (self.current_step + 1) % D;
+            (from, to)
+        }
+
+        /// this one of the optimized operators
+        /// compress the hidden state vector from the from index to the to index
+        fn merge_hidden_states(&mut self, from: usize, to: usize) {
+            for inp in 0..I {
+                self.hidden_state[to][inp] *= self.alpha_vector[inp];
+                self.hidden_state[to][inp] += ( 1.0 - self.alpha_vector[inp] )* self.hidden_state[from][inp];
+            }
+        }
+
     }
     
-    impl<const I: usize, const O: usize> NeuralNetwork<I, O> for LNNLayer<I, O> {
+    impl<const I: usize, const O: usize, const D: usize> NeuralNetwork<I, O> for LNNLayer<I, O, D> {
+
         fn forward(&mut self, x: &[f64; I]) -> [f64; O] {
+            let x = self.preprocess(x);
             let mut sum = self.bias;
+
+            // activate on alpha value to normalize
+            let alpha = (self.activation)(self.alpha());
+
             for neuron in 0..O {
                 for inp in 0..I {
-                    sum[neuron] += self.weights[neuron][inp] * x[inp];
+                    //here we account for the inputs
+                    sum[neuron] += (1.0 - alpha) * self.weights[neuron][inp] * x[inp];
+                    //here we account the D hidden states
+                    for hi in 0..D {
+                        sum[neuron] += self.hidden_state[hi][inp] * alpha / D as f64;
+                    }
                 }
-                sum[neuron] = self.hidden_state[neuron] * self.alpha + (1.0 - self.alpha) * sum[neuron];
+                //activate
                 sum[neuron] = (self.activation)(sum[neuron]);
             }
-            self.hidden_state = sum;
+
+            let (head,tail) = self.get_ring_indices();
+            self.merge_hidden_states(head, tail);
+            self.hidden_state[head] = x;
+            self.current_step +=1;
             sum
         }
         
         fn backward(&mut self, x: &[f64; I], errors: [f64; O], temperature: f64) -> [f64; I] {
-            let predict = self.forward(x);
+            let sample = self.preprocess(x);
+            let predict = self.forward(&sample);
+
             let mut correction = [0.0; I];
+            let alpha = self.alpha(); // the magnitude of accumulation
             
             for neuron in 0..O {
                 let delta = errors[neuron] * self.derivative(predict[neuron]);
                 for input_no in 0..I {
-                    correction[input_no] += delta * self.weights[neuron][input_no];
-                    self.weights[neuron][input_no] += temperature * delta * x[input_no];
+                    // the correction for the weigths
+                    let static_delta = delta * (1.0 - (self.activation)(alpha));
+                    // the correction for the hidden states
+                    let recurrent_delta = delta * (self.activation)(alpha);
+
+                    //total correction accounting
+                    correction[input_no] += static_delta * self.weights[neuron][input_no];
+                    correction[input_no] += recurrent_delta * self.alpha_vector[input_no];
+
+                    //update the weights
+                    self.weights[neuron][input_no] += temperature * static_delta * sample[input_no];
+                    self.alpha_vector[input_no] += temperature * recurrent_delta * self.alpha_vector[input_no];
                 }
                 self.bias[neuron] += temperature * delta;
             }
@@ -169,26 +294,25 @@ pub mod lnn_layer {
 }
 
 
-pub struct TrainParams {
+pub struct TrainParams<const O: usize> {
     pub epochs: usize,
     pub temperature: f64,
     pub cutoff: f64,
+    pub fn_loss: fn(&[f64; O], &[f64; O]) -> [f64;O],
 }
 
 pub fn train<const I: usize, const O: usize>(
     model: &mut impl NeuralNetwork<I, O>,
     data: &[([f64; I], [f64; O])],
-    params: TrainParams,
+    params: TrainParams<O>,
 ) -> usize {
     for cnt in 0..params.epochs {
         let mut err_acc = 0.0;
         for (sample, target) in data {
             let predict = model.forward(&sample);
-            let mut error = [0.0; O];
+            let error = (params.fn_loss)(target, &predict);
             for i in 0..O {
-                let err =  2.0 * (target[i] - predict[i]);
-                err_acc += err * err;
-                error[i] = err;
+                err_acc += error[i] * error[i];
             }
             model.backward(&sample, error, params.temperature);
         }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,75 @@
+use neural_nets_toys::*;
+
 fn main() {
+
+    fn sigmoid(x: f64) -> f64 {
+        1.0 / (1.0 + (-x).exp())
+    }
+
+    fn sigmoid_derivative(x: f64) -> f64 {
+        let v = sigmoid(x);
+        v * (1.0 - v)
+    }
+
+    let mut model = attention::Attention::<5,2>::random(
+        -1.0..1.0,
+        )
+    .chain(
+        lnn_exp::LNNLayer::<5,3,3>::random(
+            sigmoid,
+            -1.0..1.0,
+        ).with_derivative(sigmoid_derivative)
+    )
+    .chain(fc_layer::Layer::<3,1>::random(
+        sigmoid,
+        -1.0..1.0,
+    ).with_derivative(sigmoid_derivative));
+
+
+    let data = [
+        (
+            [0.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0],
+        ),
+        (
+            [1.0, 0.0, 0.0, 0.0, 0.0],
+            [1.0],
+        ),
+        (
+            [0.0, 1.0, 0.0, 0.0, 0.0],
+            [1.0],
+        ),
+        (
+            [0.0, 0.0, 1.0, 0.0, 0.0],
+            [1.0],
+        ),
+        (
+            [0.0, 0.0, 0.0, 1.0, 0.0],
+            [1.0],
+        ),
+        (
+            [0.0, 0.0, 0.0, 0.0, 1.0],
+            [1.0],
+        ),
+    ];
+
+
+    train(&mut model, &data, TrainParams {
+        epochs: 1000,
+        temperature: 0.1,
+        cutoff: 0.1,
+        fn_loss: |t,p| [
+            t.iter().zip(p.iter())
+            .map(|(t,p)| (t-p).powi(2))
+            .sum::<f64>()
+            ;1
+        ],
+    });
+
+    for (x,_) in data.iter() {
+        let y = model.forward(x);
+        println!("{:?} -> {:?}", x, y);
+    }
+
     println!("Hello, world!");
 }
